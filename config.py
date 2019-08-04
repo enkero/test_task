4ea89053609984e0b2f70f01b4e4b1b89cb376de
Enke <enke@MacBook-Pro-Enke.local>
2019-08-04 13:39:44 +0300

    fix style

--- config.py
+++ config.py
@@ -1,31 +1,22 @@
 import math
 import string
 
-      
+
 class Config():
-    temp_directory='temp/'
-    generated_file='test.txt'
-    sorted_file='sorted.txt'
-    
-    n_lines=20
-    ratio_random_lines=0.5
-    n_uniform_lines_for_each_buffer=int(n_lines*ratio_random_lines/2)
-    n_random_lines=n_lines-n_uniform_lines_for_each_buffer*2
-   
-    max_line_length=97
-    
-    max_buffer_length=10 ######### These two parameters can be changed ###########
-    max_symbols_in_memory=50 ##### depends on available RAM and strings type #####
+    temp_directory = 'temp/'
+    generated_file = 'test.txt'
+    sorted_file = 'sorted.txt'
 
+    n_lines = 10000
+    ratio_random_lines = 0.5
+    n_uniform_lines_for_each_buffer = int(n_lines*ratio_random_lines/2)
+    n_random_lines = n_lines-n_uniform_lines_for_each_buffer*2
 
-    symbols=string.ascii_letters + string.digits + ' '
-    n_buffers_per_line=math.ceil(max_line_length/max_buffer_length)
-    n_buffers_per_batch=math.floor(max_symbols_in_memory/max_buffer_length)
+    max_line_length = 97
 
+    max_buffer_length = 10  # This can be changed depends on strings type
+    max_symbols_in_memory = 1000000000
 
-
-
-
-
-
-
+    symbols = string.ascii_letters + string.digits + ' '
+    n_buffers_per_line = math.ceil(max_line_length/max_buffer_length)
+    n_buffers_per_batch = math.floor(max_symbols_in_memory/max_buffer_length)
--- test.py
+++ test.py
@@ -1,36 +1,46 @@
 import numpy as np
 import mmap
 from config import Config as cfg
-from functions import *
-
+from functions import (create_temp_directory, generate_test_file,
+                       sort_by_batches, open_sorted_batches,
+                       merge_and_sort_batches, is_adjacent_lines_are_sorted,
+                       hashed, count_lines)
 
 
 create_temp_directory(cfg.temp_directory)
 
-generate_test_file(cfg.generated_file,cfg.n_random_lines,cfg.max_line_length,cfg.max_buffer_length,cfg.symbols,cfg.n_uniform_lines_for_each_buffer,cfg.n_buffers_per_line)   
+generate_test_file(cfg.generated_file, cfg.n_random_lines,
+                   cfg.max_line_length, cfg.max_buffer_length, cfg.symbols,
+                   cfg.n_uniform_lines_for_each_buffer, cfg.n_buffers_per_line)
 
-sort_by_batches(cfg.generated_file,cfg.n_buffers_per_batch,cfg.max_buffer_length,cfg.max_symbols_in_memory,cfg.temp_directory)
+sort_by_batches(cfg.generated_file, cfg.n_buffers_per_batch,
+                cfg.max_buffer_length, cfg.max_symbols_in_memory,
+                cfg.temp_directory)
 
-opened,mmaped,starts,ends=open_sorted_batches(cfg.temp_directory)
+opened, mmaped, starts, ends = open_sorted_batches(cfg.temp_directory)
 
-merge_and_sort_batches(opened,mmaped,starts,ends,cfg.sorted_file,cfg.max_buffer_length,cfg.max_symbols_in_memory,cfg.temp_directory)
+merge_and_sort_batches(opened, mmaped, starts, ends,
+                       cfg.sorted_file, cfg.max_buffer_length,
+                       cfg.max_symbols_in_memory, cfg.temp_directory)
 
 
-
-file = open(cfg.generated_file,'r')
+file = open(cfg.generated_file, 'r')
 mmaped = mmap.mmap(file.fileno(), 0, access=mmap.ACCESS_READ)
 
-file_sorted = open(cfg.sorted_file,'r')
+file_sorted = open(cfg.sorted_file, 'r')
 mmaped_sorted = mmap.mmap(file_sorted.fileno(), 0, access=mmap.ACCESS_READ)
 
 
-
 def test_equal_sizes():
-    assert len(mmaped)==len(mmaped_sorted)
-    
+    assert len(mmaped) == len(mmaped_sorted)
+
+
 def test_equal_hashes():
-    assert hashed(mmaped,cfg.max_symbols_in_memory)==hashed(mmaped_sorted,cfg.max_symbols_in_memory)
-    
+    assert (hashed(mmaped, cfg.max_symbols_in_memory)
+            == hashed(mmaped_sorted, cfg.max_symbols_in_memory))
+
+
 def test_lines_sorted():
-    assert np.sum(is_adjacent_lines_are_sorted(mmaped_sorted,cfg.max_buffer_length))==count_lines(mmaped_sorted)-1    
-    
+    assert (np.sum(is_adjacent_lines_are_sorted(mmaped_sorted,
+                                                cfg.max_buffer_length))
+            == count_lines(mmaped_sorted)-1)
